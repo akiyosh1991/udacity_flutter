0d9eb459d0f22e78f28ae9d98853fab1b39df1b3
YAMAMOTOAKIYOSHI <yamamoto@mediaxis.jp>
2019-07-29 23:51:45 +0900

    レイアウト練習

--- lib/UI/Pages/Home.dart
+++ lib/UI/Pages/Home.dart
@@ -26,8 +26,95 @@ class Home extends StatelessWidget {
 }
 
 class HomeListItem extends StatelessWidget {
+  var Content = Column(
+    children: <Widget>[
+      Expanded(
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          Text('TestUser'),
+                          Text('@test_user・2019-07-07')
+                        ],
+                      ),
+                    ),
+                   Row(children: <Widget>[
+                     Text('レイアウトの練習でええす。')
+                   ],)
+
+                  ],
+                )
+            ),
+            Icon(Icons.expand_more, size: 24.0,)
+          ],
+        ),
+      ),
+      ),
+      Container(
+        height: 25.0,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+               child: FittedBox(
+                 fit: BoxFit.contain,
+                 child: Icon(Icons.comment),
+               ),
+            ),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Icon(Icons.favorite),
+              ),
+            ),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Icon(Icons.file_download),
+              ),
+            ),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Icon(Icons.share),
+              ),
+            ),
+            Expanded(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Icon(Icons.group_work),
+              ),
+            ),
+          ],
+        ),
+      )
+    ],
+  );
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Colors.grey
+        ),
+      ),
+      height:100.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.account_circle,size: 60.0),
+            Expanded(
+              child: Content,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
