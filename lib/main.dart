fcc9daf303661246376b1f1dd5e8fdd8b73f8bd0
YAMAMOTOAKIYOSHI <yamamoto@mediaxis.jp>
2019-08-23 00:31:33 +0900

    feat: fade_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,46 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-// TODO: Import the CategoryRoute widget
-import 'category_route.dart';
-/// The function that is called when main.dart is run.
 void main() {
-  runApp(UnitConverterApp());
+  runApp(MyHomePage());
 }
 
-/// This widget is the root of our application.
-///
-/// The first screen we see is a list [Categories].
-class UnitConverterApp extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _visible = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
-      // our home should now point to an instance of the CategoryRoute widget.
-      home: CategoryRoute(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Home Page"),
+        ),
+        body: Center(
+          child:AnimatedOpacity(
+            opacity: _visible ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 500),
+            child: Container(
+              width: 200.0,
+              height: 200.0,
+              color: Colors.green,
+            ),
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.flip),
+          onPressed: () {
+            setState(() {
+              _visible = !_visible;
+            });
+          },
+          tooltip: 'Toggle Opacity',
+        ),
+      ),
     );
   }
 }
