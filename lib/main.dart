3dadb5485d29cf46eae77abff4e1dcc958607e5b
YAMAMOTOAKIYOSHI <yamamoto@mediaxis.jp>
2019-09-04 22:30:04 +0900

    feat: Orientation Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,47 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-// TODO: Import the CategoryRoute widget
-import 'category_route.dart';
 /// The function that is called when main.dart is run.
 void main() {
-  runApp(UnitConverterApp());
+  runApp(MyApp());
 }
 
-/// This widget is the root of our application.
-///
-/// The first screen we see is a list [Categories].
-class UnitConverterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'Orientation Demo';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
-      // our home should now point to an instance of the CategoryRoute widget.
-      home: CategoryRoute(),
+      title: appTitle,
+      home: OrientationList(
+        title: appTitle
+      ),
+    );
+  }
+}
+
+class OrientationList extends StatelessWidget {
+  final String title;
+
+  OrientationList({Key key, this.title}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(crossAxisCount: orientation == Orientation.portrait ? 4 : 3,
+          children: List.generate(1000, (index){
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.caption,
+              ),
+            );
+          }),
+          );
+        }
+      )
     );
   }
 }
