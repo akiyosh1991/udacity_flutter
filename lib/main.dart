2e80569bebfa4c7f90d54c1e3953715d2b61b191
YAMAMOTOAKIYOSHI <yamamoto@mediaxis.jp>
2019-09-06 22:53:15 +0900

    feat: tabViewController

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-// TODO: Import the CategoryRoute widget
-import 'category_route.dart';
-/// The function that is called when main.dart is run.
 void main() {
-  runApp(UnitConverterApp());
+  runApp(TabBarDemo());
 }
 
-/// This widget is the root of our application.
-///
-/// The first screen we see is a list [Categories].
-class UnitConverterApp extends StatelessWidget {
+class TabBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
-      // our home should now point to an instance of the CategoryRoute widget.
-      home: CategoryRoute(),
+      title: 'Tab Bar Demo',
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.directions_bus)),
+              ],
+            ),
+            title: Text("Tab Bar Demo"),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_bike),
+              Icon(Icons.directions_bus),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
