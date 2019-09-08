fcb455b3b22d4db8de326fe6e30e76627ca8ce4c
YAMAMOTOAKIYOSHI <yamamoto@mediaxis.jp>
2019-09-08 21:01:37 +0900

    feat: form with validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,66 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-// TODO: Import the CategoryRoute widget
-import 'category_route.dart';
-/// The function that is called when main.dart is run.
 void main() {
-  runApp(UnitConverterApp());
+  runApp(MyApp());
 }
 
-/// This widget is the root of our application.
-///
-/// The first screen we see is a list [Categories].
-class UnitConverterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
-      // our home should now point to an instance of the CategoryRoute widget.
-      home: CategoryRoute(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Form'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MyCustomForm(),
+        ),
+      ),
     );
   }
 }
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value){
+              if (value.isEmpty) {
+                return "please enter some text";
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding:const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if(_formKey.currentState.validate()) {
+                  Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text("submit"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
